ec68583f4fe9eb01549005c82166a4a171e2659d
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-02-27 16:06:27 +0700

    add relationship between user & micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,4 @@
 class Micropost < ApplicationRecord
   validates :content, presence: true, length: { minimum: 5};
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true;
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP };
+  has_many :microposts
 end
