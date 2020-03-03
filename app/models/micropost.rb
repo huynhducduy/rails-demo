ebd4a22f8dc72d35a2957d0f7b27fcad779fba37
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-03 15:12:33 +0700

    add relationship betweeen micropost - category

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   validates :content, presence: true, length: { minimum: 5};
   belongs_to :user
+  belongs_to :category
   has_rich_text :content
   has_many_attached :images
 
